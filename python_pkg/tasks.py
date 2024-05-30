d239d7193504667f41f2ba0039671bcc85701bb0
lucasallison <lucasgallison@gmail.com>
2024-05-30 18:12:41 +0200

    Changes in order to run on M2 macbook

--- python_pkg/tasks.py
+++ python_pkg/tasks.py
@@ -21,13 +21,14 @@ def build_quasimodo(c):
 
 # Use -undefined dynamic_lookup for MACOS
 def compile_python_module(cpp_name, extension_name):
+    libs = "-L/opt/homebrew/lib -L/opt/homebrew/Cellar/boost/1.85.0/lib -I/opt/homebrew/Cellar/boost/1.85.0/include -L/opt/homebrew/Cellar/mpfr/4.2.1/lib -I/opt/homebrew/Cellar/mpfr/4.2.1/include -L/opt/homebrew/Cellar/gmp/6.3.0/lib -I/opt/homebrew/Cellar/gmp/6.3.0/include"
     invoke.run(
-        "g++ -g -O3 -L/opt/homebrew/lib -std=c++2a -w -shared -Wall -Wextra -DHAVE_CONFIG_H -Werror -Wunused-but-set-variable -fPIC -I${3} "
+        "g++ -g -O3 -L/opt/homebrew/lib {4} -std=c++2a -w -shared -Wall -Wextra -DHAVE_CONFIG_H -Werror -Wunused-but-set-variable -fPIC -I${3}"
         "`python3.11 -m pybind11 --includes` "
         "-I {2} -I../ "
         "{0} "
         "-o {1}`python3.11-config --extension-suffix` "
-        "-L. -lquasimodo -Wl,-rpath,.".format(cpp_name, extension_name, os.environ["PYTHON_INCLUDE"], os.environ["BOOST_PATH"])
+        "-L. -lquasimodo -Wl,-rpath,. -undefined dynamic_lookup".format(cpp_name, extension_name, os.environ["PYTHON_INCLUDE"], os.environ["BOOST_PATH"], libs)
     )
 
 @invoke.task()
---
+++ set_env.sh
@@ -0,0 +1,2 @@
+export BOOST_PATH=/opt/homebrew/Cellar/boost/1.85.0
+export PYTHON_INCLUDE=/opt/homebrew/opt/python@3.11/Frameworks/Python.framework/Versions/3.11/include/python3.11
